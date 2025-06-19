4c734c361eda85cb2338e1524a59f7818f114525
Harpur55 <purnomo.hari3180@gmail.com>
2025-06-19 12:57:40 +0700

    upadate route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CoachController;
 
-Route::get('/', [\App\Http\Controllers\CoachController::class, 'show'])->name('coach.show');
 
-Route::get('/cek', function () {
-    return view('cek');
-});
-Route::get('/index', [\App\Http\Controllers\CoachController::class, 'show'])->name('coach.show');
+
+Route::get('/', [\App\Http\Controllers\CoachController::class, 'show'])->name('coach.home');
+Route::get('/index', [\App\Http\Controllers\CoachController::class, 'show'])->name('coach.index');
